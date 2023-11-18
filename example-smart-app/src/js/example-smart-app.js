5a52d664995007d56da1697ccc944a4f4a2e226e
Jason Wang <jason98wang@hotmail.com>
2023-11-18 18:56:02 -0500

    pushing new code

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -21,61 +21,15 @@
                       }
                     }
                   });
-  
-        // //test document data
-        //   var documentData = {
-        //     resourceType: "DocumentReference",
-        //     status: "current", // or another appropriate status
-        //     type: {
-        //       coding: [
-        //         {
-        //           system: "http://loinc.org",
-        //           code: "18842-5",
-        //           display: "Discharge summary" // Replace with appropriate display text
-        //         }
-        //       ]
-        //     },
-        //     subject: {
-        //       reference: "Patient/" + patient.id
-        //     },
-        //     content: [
-        //       {
-        //         attachment: {
-        //           contentType: "text/plain",
-        //           data: btoa("This is what I want to summarize") // Base64 encode string
-        //         }
-        //       }
-        //     ]
-        //   };//creating a document reference for the summary, this can be called later
-          
-        // createDocumentReference(smart, documentData, function(response) {
-        // console.log('DocumentReference created:', response);
-        // var createdDocumentId = response.id; // Assuming the response contains the ID
-        // // Store this ID to fetch the document later
-        // }, function(error) {
-        //   console.error('Error creating DocumentReference:', error);
-        // });
-        // console.log('DocumentReference created:', createdDocumentId);
-        
-        // var docRef = smart.patient.api.read({
-        //             type: 'DocumentReference',
-        //             id: createdDocumentId
-        //           });
-        
-        //$.when(pt, obv, docRef).fail(onError);
-        var dockRef = nil
-        $.when(pt, obv, docRef).done(function(patient, obv, docRef) {
+
+        $.when(pt, obv).fail(onError);
+
+        $.when(pt, obv).done(function(patient, obv) {
           var byCodes = smart.byCodes(obv, 'code');
           var gender = patient.gender;
 
           var fname = '';
           var lname = '';
-          // Processing for the fetched DocumentReference
-          if (docRef) {
-            console.log('Fetched DocumentReference:', docRef);
-            // Additional processing as needed
-          }
-          
 
           if (typeof patient.name[0] !== 'undefined') {
             fname = patient.name[0].given.join(' ');
@@ -87,10 +41,6 @@
           var diastolicbp = getBloodPressureValue(byCodes('55284-4'),'8462-4');
           var hdl = byCodes('2085-9');
           var ldl = byCodes('2089-1');
-          var dischargeSumary = byCodes("18842-5")
-          onsole.log("DISCHARGE SUMMAR IS", dischargeSumary)
-          console.log(patient)
-          console.log(smart)
 
           var p = defaultPatient();
           p.birthdate = patient.birthDate;
@@ -121,29 +71,6 @@
     return ret.promise();
 
   };
-    //MY FUNCTIONS
-  //used to create document reference
-  function createDocumentReference(smart, documentData, onSuccess, onError) {
-    return smart.request({
-    url: 'DocumentReference',
-    method: 'POST',
-    body: JSON.stringify(documentData),
-    headers: {
-      'Content-Type': 'application/fhir+json'
-    }
-  }).then(response => response.json())
-    .catch(error => console.error('Error creating DocumentReference:', error));
-  }
-  //function to fetch the document reference to summaries
-  function fetchDocumentReference(smart, documentId) {
-    smart.request(`DocumentReference/${documentId}`, {
-      method: 'GET'
-    }).then(function(documentReference) {
-      console.log('Fetched DocumentReference returned value:', documentReference);
-    }).catch(function(error) {
-      console.error('Error fetching DocumentReference:', error);
-    });
-  }
 
   function defaultPatient(){
     return {
@@ -202,3 +129,206 @@
   };
 
 })(window);
+// (function(window){
+//   window.extractData = function() {
+//     var ret = $.Deferred();
+
+//     function onError() {
+//       console.log('Loading error', arguments);
+//       ret.reject();
+//     }
+
+//     function onReady(smart)  {
+//       if (smart.hasOwnProperty('patient')) {
+//         var patient = smart.patient;
+//         var pt = patient.read();
+//         var obv = smart.patient.api.fetchAll({
+//                     type: 'Observation',
+//                     query: {
+//                       code: {
+//                         $or: ['http://loinc.org|8302-2', 'http://loinc.org|8462-4',
+//                               'http://loinc.org|8480-6', 'http://loinc.org|2085-9',
+//                               'http://loinc.org|2089-1', 'http://loinc.org|55284-4']
+//                       }
+//                     }
+//                   });
+  
+//         // //test document data
+//         //   var documentData = {
+//         //     resourceType: "DocumentReference",
+//         //     status: "current", // or another appropriate status
+//         //     type: {
+//         //       coding: [
+//         //         {
+//         //           system: "http://loinc.org",
+//         //           code: "18842-5",
+//         //           display: "Discharge summary" // Replace with appropriate display text
+//         //         }
+//         //       ]
+//         //     },
+//         //     subject: {
+//         //       reference: "Patient/" + patient.id
+//         //     },
+//         //     content: [
+//         //       {
+//         //         attachment: {
+//         //           contentType: "text/plain",
+//         //           data: btoa("This is what I want to summarize") // Base64 encode string
+//         //         }
+//         //       }
+//         //     ]
+//         //   };//creating a document reference for the summary, this can be called later
+          
+//         // createDocumentReference(smart, documentData, function(response) {
+//         // console.log('DocumentReference created:', response);
+//         // var createdDocumentId = response.id; // Assuming the response contains the ID
+//         // // Store this ID to fetch the document later
+//         // }, function(error) {
+//         //   console.error('Error creating DocumentReference:', error);
+//         // });
+//         // console.log('DocumentReference created:', createdDocumentId);
+        
+//         // var docRef = smart.patient.api.read({
+//         //             type: 'DocumentReference',
+//         //             id: createdDocumentId
+//         //           });
+        
+//         //$.when(pt, obv, docRef).fail(onError);
+//         $.when(pt, obv, docRef).done(function(patient, obv, docRef) {
+//           var byCodes = smart.byCodes(obv, 'code');
+//           var gender = patient.gender;
+
+//           var fname = '';
+//           var lname = '';
+//           // Processing for the fetched DocumentReference
+//           if (docRef) {
+//             console.log('Fetched DocumentReference:', docRef);
+//             // Additional processing as needed
+//           }
+          
+
+//           if (typeof patient.name[0] !== 'undefined') {
+//             fname = patient.name[0].given.join(' ');
+//             lname = patient.name[0].family.join(' ');
+//           }
+
+//           var height = byCodes('8302-2');
+//           var systolicbp = getBloodPressureValue(byCodes('55284-4'),'8480-6');
+//           var diastolicbp = getBloodPressureValue(byCodes('55284-4'),'8462-4');
+//           var hdl = byCodes('2085-9');
+//           var ldl = byCodes('2089-1');
+//           var dischargeSumary = byCodes("18842-5")
+//           onsole.log("DISCHARGE SUMMAR IS", dischargeSumary)
+//           console.log(patient)
+//           console.log(smart)
+
+//           var p = defaultPatient();
+//           p.birthdate = patient.birthDate;
+//           p.gender = gender;
+//           p.fname = fname;
+//           p.lname = lname;
+//           p.height = getQuantityValueAndUnit(height[0]);
+
+//           if (typeof systolicbp != 'undefined')  {
+//             p.systolicbp = systolicbp;
+//           }
+
+//           if (typeof diastolicbp != 'undefined') {
+//             p.diastolicbp = diastolicbp;
+//           }
+
+//           p.hdl = getQuantityValueAndUnit(hdl[0]);
+//           p.ldl = getQuantityValueAndUnit(ldl[0]);
+
+//           ret.resolve(p);
+//         });
+//       } else {
+//         onError();
+//       }
+//     }
+
+//     FHIR.oauth2.ready(onReady, onError);
+//     return ret.promise();
+
+//   };
+//     //MY FUNCTIONS
+//   //used to create document reference
+//   function createDocumentReference(smart, documentData, onSuccess, onError) {
+//     return smart.request({
+//       url: 'DocumentReference',
+//       method: 'POST',
+//       body: JSON.stringify(documentData),
+//       headers: {
+//         'Content-Type': 'application/fhir+json'
+//       }
+//   }).then(response => response.json())
+//     .catch(error => console.error('Error creating DocumentReference:', error));
+//   }
+//   //function to fetch the document reference to summaries
+//   function fetchDocumentReference(smart, documentId) {
+//     smart.request(`DocumentReference/${documentId}`, {
+//       method: 'GET'
+//     }).then(function(documentReference) {
+//       console.log('Fetched DocumentReference returned value:', documentReference);
+//     }).catch(function(error) {
+//       console.error('Error fetching DocumentReference:', error);
+//     });
+//   }
+
+//   function defaultPatient(){
+//     return {
+//       fname: {value: ''},
+//       lname: {value: ''},
+//       gender: {value: ''},
+//       birthdate: {value: ''},
+//       height: {value: ''},
+//       systolicbp: {value: ''},
+//       diastolicbp: {value: ''},
+//       ldl: {value: ''},
+//       hdl: {value: ''},
+//     };
+//   }
+
+//   function getBloodPressureValue(BPObservations, typeOfPressure) {
+//     var formattedBPObservations = [];
+//     BPObservations.forEach(function(observation){
+//       var BP = observation.component.find(function(component){
+//         return component.code.coding.find(function(coding) {
+//           return coding.code == typeOfPressure;
+//         });
+//       });
+//       if (BP) {
+//         observation.valueQuantity = BP.valueQuantity;
+//         formattedBPObservations.push(observation);
+//       }
+//     });
+
+//     return getQuantityValueAndUnit(formattedBPObservations[0]);
+//   }
+
+//   function getQuantityValueAndUnit(ob) {
+//     if (typeof ob != 'undefined' &&
+//         typeof ob.valueQuantity != 'undefined' &&
+//         typeof ob.valueQuantity.value != 'undefined' &&
+//         typeof ob.valueQuantity.unit != 'undefined') {
+//           return ob.valueQuantity.value + ' ' + ob.valueQuantity.unit;
+//     } else {
+//       return undefined;
+//     }
+//   }
+
+//   window.drawVisualization = function(p) {
+//     $('#holder').show();
+//     $('#loading').hide();
+//     $('#fname').html(p.fname);
+//     $('#lname').html(p.lname);
+//     $('#gender').html(p.gender);
+//     $('#birthdate').html(p.birthdate);
+//     $('#height').html(p.height);
+//     $('#systolicbp').html(p.systolicbp);
+//     $('#diastolicbp').html(p.diastolicbp);
+//     $('#ldl').html(p.ldl);
+//     $('#hdl').html(p.hdl);
+//   };
+
+// })(window);
